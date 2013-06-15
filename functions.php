47ab9bba8daebaacf6edf104a2374c5e6f41a06d
Sunny Ratilal <sunnyratilal5@gmail.com>
2013-06-15 07:25:15 +0100

    Update files for EDD

--- functions.php
+++ functions.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * _s functions and definitions
+ * EDD functions and definitions
  *
- * @package _s
+ * @package EDD
  */
 
 /**
@@ -11,7 +11,7 @@
 if ( ! isset( $content_width ) )
 	$content_width = 640; /* pixels */
 
-if ( ! function_exists( '_s_setup' ) ) :
+if ( ! function_exists( 'edd_setup' ) ) :
 /**
  * Sets up theme defaults and registers support for various WordPress features.
  *
@@ -19,15 +19,15 @@ if ( ! function_exists( '_s_setup' ) ) :
  * before the init hook. The init hook is too late for some features, such as indicating
  * support post thumbnails.
  */
-function _s_setup() {
+function edd_setup() {
 
 	/**
 	 * Make theme available for translation
 	 * Translations can be filed in the /languages/ directory
-	 * If you're building a theme based on _s, use a find and replace
-	 * to change '_s' to the name of your theme in all the template files
+	 * If you're building a theme based on EDD, use a find and replace
+	 * to change 'edd' to the name of your theme in all the template files
 	 */
-	load_theme_textdomain( '_s', get_template_directory() . '/languages' );
+	load_theme_textdomain( 'edd', get_template_directory() . '/languages' );
 
 	/**
 	 * Add default posts and comments RSS feed links to head
@@ -45,7 +45,7 @@ function _s_setup() {
 	 * This theme uses wp_nav_menu() in one location.
 	 */
 	register_nav_menus( array(
-		'primary' => __( 'Primary Menu', '_s' ),
+		'primary' => __( 'Primary Menu', 'edd' ),
 	) );
 
 	/**
@@ -53,8 +53,8 @@ function _s_setup() {
 	 */
 	add_theme_support( 'post-formats', array( 'aside', 'image', 'video', 'quote', 'link' ) );
 }
-endif; // _s_setup
-add_action( 'after_setup_theme', '_s_setup' );
+endif; // edd_setup
+add_action( 'after_setup_theme', 'edd_setup' );
 
 /**
  * Setup the WordPress core custom background feature.
@@ -68,13 +68,13 @@ add_action( 'after_setup_theme', '_s_setup' );
  *
  * Hooks into the after_setup_theme action.
  */
-function _s_register_custom_background() {
+function edd_register_custom_background() {
 	$args = array(
 		'default-color' => 'ffffff',
 		'default-image' => '',
 	);
 
-	$args = apply_filters( '_s_custom_background_args', $args );
+	$args = apply_filters( 'edd_custom_background_args', $args );
 
 	if ( function_exists( 'wp_get_theme' ) ) {
 		add_theme_support( 'custom-background', $args );
@@ -85,14 +85,14 @@ function _s_register_custom_background() {
 		add_custom_background();
 	}
 }
-add_action( 'after_setup_theme', '_s_register_custom_background' );
+add_action( 'after_setup_theme', 'edd_register_custom_background' );
 
 /**
  * Register widgetized area and update sidebar with default widgets
  */
-function _s_widgets_init() {
+function edd_widgets_init() {
 	register_sidebar( array(
-		'name'          => __( 'Sidebar', '_s' ),
+		'name'          => __( 'Sidebar', 'edd' ),
 		'id'            => 'sidebar-1',
 		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
 		'after_widget'  => '</aside>',
@@ -100,27 +100,27 @@ function _s_widgets_init() {
 		'after_title'   => '</h1>',
 	) );
 }
-add_action( 'widgets_init', '_s_widgets_init' );
+add_action( 'widgets_init', 'edd_widgets_init' );
 
 /**
  * Enqueue scripts and styles
  */
-function _s_scripts() {
-	wp_enqueue_style( '_s-style', get_stylesheet_uri() );
+function edd_scripts() {
+	wp_enqueue_style( 'edd-style', get_stylesheet_uri() );
 
-	wp_enqueue_script( '_s-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
+	wp_enqueue_script( 'edd-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
 
-	wp_enqueue_script( '_s-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
+	wp_enqueue_script( 'edd-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
 
 	if ( is_singular() && wp_attachment_is_image() ) {
-		wp_enqueue_script( '_s-keyboard-image-navigation', get_template_directory_uri() . '/js/keyboard-image-navigation.js', array( 'jquery' ), '20120202' );
+		wp_enqueue_script( 'edd-keyboard-image-navigation', get_template_directory_uri() . '/js/keyboard-image-navigation.js', array( 'jquery' ), '20120202' );
 	}
 }
-add_action( 'wp_enqueue_scripts', '_s_scripts' );
+add_action( 'wp_enqueue_scripts', 'edd_scripts' );
 
 /**
  * Custom template tags for this theme.
