87ca94bfb3417d48c8b42f5abd478bbd905e0e5b
Sunny Ratilal <sunnyratilal5@gmail.com>
2013-06-23 15:27:50 -0400

    Clean up search results page

--- no-results.php
+++ no-results.php
@@ -14,20 +14,12 @@
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
-		<?php if ( is_home() && current_user_can( 'publish_posts' ) ) : ?>
-
-			<p><?php printf( __( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'edd' ), esc_url( admin_url( 'post-new.php' ) ) ); ?></p>
-
-		<?php elseif ( is_search() ) : ?>
-
-			<p><?php _e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'edd' ); ?></p>
-			<?php get_search_form(); ?>
-
-		<?php else : ?>
-
-			<p><?php _e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'edd' ); ?></p>
-			<?php get_search_form(); ?>
-
-		<?php endif; ?>
+		<?php
+		if ( is_search() ) {
+			printf( '<p>%s</p>', _e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'edd' ) );
+		} else {
+			printf( '<p>%s</p>', _e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'edd' ) . get_search_form() );
+		}
+		?>
 	</div><!-- .entry-content -->
 </article><!-- #post-0 .post .no-results .not-found -->
