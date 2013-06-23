a63420191c82c0b869b85bacc5ab45c6d6b9d9c6
Sunny Ratilal <sunnyratilal5@gmail.com>
2013-06-23 15:56:42 -0400

    Add base features template

---
+++ template-features.php
@@ -0,0 +1,16 @@
+<?php
+/* Template Name: Features */
+get_header();
+?>
+
+	<section class="headline">
+		<?php printf( '<h1>%s</h1>', the_content() ); ?>
+	</section>
+
+	<?php
+
+	// Load Features
+
+	?>
+
+<?php get_footer(); ?>
