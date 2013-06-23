03500988c47db1b3c01165a4718dd3a9a51e7581
Sunny Ratilal <sunnyratilal5@gmail.com>
2013-06-23 15:39:23 -0400

    Register sidebars

--- functions.php
+++ functions.php
@@ -33,8 +33,26 @@ add_action( 'after_setup_theme', 'edd_theme_setup' );
  */
 function edd_register_theme_widgets() {
 	register_sidebar( array(
-		'name'          => __( 'Sidebar', 'edd' ),
-		'id'            => 'sidebar-1',
+		'name'          => __( 'Blog Sidebar', 'edd' ),
+		'id'            => 'blog-sidebar',
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</aside>',
+		'before_title'  => '<h1 class="widget-title">',
+		'after_title'   => '</h1>',
+	) );
+
+	register_sidebar( array(
+		'name'          => __( 'Forums Sidebar', 'edd' ),
+		'id'            => 'forum-sidebar',
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</aside>',
+		'before_title'  => '<h1 class="widget-title">',
+		'after_title'   => '</h1>',
+	) );
+
+	register_sidebar( array(
+		'name'          => __( 'Documentation Sidebar', 'edd' ),
+		'id'            => 'documentation-sidebar',
 		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
 		'after_widget'  => '</aside>',
 		'before_title'  => '<h1 class="widget-title">',
