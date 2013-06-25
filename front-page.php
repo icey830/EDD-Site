945a0c5e26a9cc8d590a325f743853eb73c37369
Sunny Ratilal <sunnyratilal5@gmail.com>
2013-06-25 14:41:27 -0500

    Add the hero

--- front-page.php
+++ front-page.php
@@ -1,5 +1,28 @@
 <?php get_header(); ?>
 
-	
+<?php
+/* ----------------------------- *
+ * Hero
+ * ----------------------------- */
+?>
+<div class="hero-container clearfix">
+	<section class="hero clearfix">
+		<div class="hero-inside">
+			<div class="hero-text">
+				<h1>Say hello to the world's easiest way to sell digital downloads through WordPress</h1>
+				<h2>for free</h2>
+				<p><a href="http://downloads.wordpress.org/plugin/easy-digital-downloads.latest-stable.zip" class="download-button"><i class="icon-cloud-download"></i> Download</a> <a href="https://easydigitaldownloads.com/demo/" class="download-button"><i class="icon-eye-open"></i> Demo</a></p>
+			</div><!-- /.hero-text -->
+			<img src="<?php echo get_template_directory_uri(); ?>/images/edd-standing.png" />
+		</div><!-- .hero-inside -->
+		<div id="video-container"></div><!-- #video-container -->
+		<div id="video-complete">
+			<div id="video-complete-inner">
+					<a class="replay-button"><span class="button"></span> Replay</a>
+				<a class="close-button"><span class="button"></span> Close</a>
+			</div><!-- #video-complete-inner -->
+		</div><!-- /#video-complete -->
+	</section><!-- /.hero -->
+</div><!-- /.hero-container -->
 
 <?php get_footer(); ?>
