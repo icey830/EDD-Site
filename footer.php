c4d4b3195c55cac28819a20a88a07021b3279e01
Sunny Ratilal <sunnyratilal5@gmail.com>
2013-06-25 15:15:02 -0500

    Show the latest post and add social buttons

--- footer.php
+++ footer.php
@@ -12,7 +12,9 @@
 		<div class="container">
 			<img src="<?php echo get_template_directory_uri(); ?>/images/edd-sitting.png" />
 			<div class="colums-clearfix">
-				<?php eddwp_get_latest_post(); ?>
+				<div class="dev-blog col">
+					<?php eddwp_get_latest_post(); ?>
+				</div>
 
 				<div class="forum col">
 					<h4>Need help?</h4>
@@ -26,8 +28,16 @@
 				</div>
 			</div>
 
+			<div class="social clearfix">
+				<ul>
+					<li class="facebook"><a href="#"></a></li>
+					<li class="twitter"><a href="#"></a></li>
+					<li class="gplus"><a href="#"></a></li>
+					<li class="github"><a href="#"></a></li>
+				</ul>
+			</div>
+
 			<?php
-			eddwp_get_social_options();
 			eddwp_get_footer_nav();
 			?>
 			<p class="copyright">Copyright &copy; 2013, Easy Digital Downloads. A project by <a href="<?php echo esc_url( '/team/' ); ?>">Pippin Williamson and Friends</a>.</p>
--- functions.php
+++ functions.php
@@ -143,7 +143,20 @@ function edd_image_full_quality( $quality ) {
 add_filter( 'jpeg_quality', 'edd_image_full_quality' );
 add_filter( 'wp_editor_set_quality', 'edd_image_full_quality' );
 
-function eddwp_get_latest_post() {  }
+function eddwp_get_latest_post() {
+	$query = new WP_Query(
+		array(
+			'posts_per_page' => 1
+		)
+	);
+	
+	while ( $query->have_posts() ) {
+		$query->the_post();
+		printf( '<h4>%s</h4>', get_the_title() );
+		the_excerpt();
+		printf( '<a href="%1$s">%2$s</a>', get_permalink(), __( 'Read More...', 'edd' ) );
+	}
+}
 
 function eddwp_get_social_options() {  }
 
