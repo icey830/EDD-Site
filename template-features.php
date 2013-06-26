8434a3af2fa586f4ee8b0dd9ff6af800f07fd7f2
Sunny Ratilal <sunnyratilal5@gmail.com>
2013-06-26 11:21:14 -0500

    Finish up features page

--- template-features.php
+++ template-features.php
@@ -1,16 +1,39 @@
 <?php
 /* Template Name: Features */
 get_header();
+the_post();
 ?>
 
 	<section class="headline">
-		<?php printf( '<h1>%s</h1>', the_content() ); ?>
+		<h1><?php the_content(); ?></h1>
 	</section>
 
 	<?php
-
-	// Load Features
-
+	$features = new WP_Query( array( 'post_type' => 'features', 'nopaging' => true, 'order' => 'ASC' ) );
+	
+	while ( $features->have_posts() ) {
+		$features->the_post();
+		?>
+		<section class="feature-area <?php echo ( 'Yes' == get_post_meta( get_the_ID(), 'ecpt_padding', true ) ) ? 'feature-pad' : null; ?> <?php echo 'feature-' . get_post_meta( get_the_ID(), 'ecpt_position', true ); ?> feature-<?php the_ID(); ?>">
+			<section class="feature-text">
+				<div class="feature-text-inner">
+					<h3><?php the_title(); ?></h3>
+					<?php the_content(); ?>
+				</div><!-- /.feature-text-inner -->
+				<?php the_post_thumbnail(); ?>
+			</section><!-- /.feature-text -->
+		</section><!-- /.feature-area -->
+		<?php
+	}
+	
+	wp_reset_postdata();
 	?>
+	<section class="feature-area" id="feature-edd">
+		<section class="feature-text">
+			<div class="feature-text-inner">
+				<h3>Not sure yet? Come by <a href="support.html">Support</a> and ask anything you want!</h3>
+			</div><!-- /.feature-text-inner -->
+		</section><!-- /.feature-text -->
+	</section><!-- /.feature-area -->
 
 <?php get_footer(); ?>
