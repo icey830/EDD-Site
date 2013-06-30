6e9e8b2622eeb48dd4fe0d9d04be7c36f92a74eb
Sunny Ratilal <sunnyratilal5@gmail.com>
2013-06-30 08:02:29 -0500

    Alter the query for the showcase.

--- template-showcase.php
+++ template-showcase.php
@@ -11,26 +11,28 @@ the_post();
 		</section><!-- /.content -->
 
 		<section class="showcase clearfix">
-			<?php
-			$showcases = new WP_Query( array( 'post_type' => 'showcase' ) );
-			
-			while ( $showcases->have_posts() ) {
-				$showcases->the_post();
-				?>
-				<div class="site">
-					<a href="<?php echo shortcode_parse_atts( get_the_content() )['link'] ?>">
-						<?php the_post_thumbnail( 'showcase' ); ?>
-					</a>
-					<a class="text-overlay" href="<?php echo shortcode_parse_atts( get_the_content() )['link'] ?>">
-						<?php the_title(); ?>
-						<span class="button">Launch Website</span>
-					</a><!-- /.text-overlay -->
-				</div><!-- /.site -->
+			<div class="sites clearfix">
 				<?php
-			}
-			
-			wp_reset_postdata();
-			?>
+				$showcases = new WP_Query( array( 'post_type' => 'showcase', 'nopaging' => true ) );
+				
+				while ( $showcases->have_posts() ) {
+					$showcases->the_post();
+					?>
+					<div class="site">
+						<a href="<?php $array = shortcode_parse_atts( get_the_content() ); echo $array['link']; ?>">
+							<?php the_post_thumbnail( 'showcase' ); ?>
+						</a>
+						<a class="text-overlay" href="<?php echo $array['link'] ?>">
+							<?php the_title(); ?>
+							<span class="button">Launch Website</span>
+						</a><!-- /.text-overlay -->
+					</div><!-- /.site -->
+					<?php
+				}
+				
+				wp_reset_postdata();
+				?>
+			</div><!-- /.sites -->
 			<div class="clearfix">
 				<?php the_content(); ?>
 			</div>
