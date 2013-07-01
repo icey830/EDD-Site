2eaf1c148a0ffe2c02e2b4f29436e95c6e97c63d
Sunny Ratilal <sunnyratilal5@gmail.com>
2013-07-01 12:26:07 -0500

    Add sidebar template for the forums

---
+++ sidebar-forums.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * The Sidebar containing the forum widget area.
+ *
+ * @package EDD
+ */
+?>
+	<aside class="sidebar">
+		<?php dynamic_sidebar( 'forum-sidebar' ); ?>
+	</aside><!-- /.sidebar -->
