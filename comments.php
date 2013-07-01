10e4b141f23c7783d21dade29feece78ba52d640
Sunny Ratilal <sunnyratilal5@gmail.com>
2013-07-01 12:24:48 -0500

    Add comments template

--- comments.php
+++ comments.php
@@ -1,11 +1,6 @@
 <?php
 /**
- * The template for displaying Comments.
- *
- * The area of the page that contains both current comments
- * and the comment form. The actual display of comments is
- * handled by a callback to edd_comment() which is
- * located in the inc/template-tags.php file.
+ * The template for displaying comments.
  *
  * @package EDD
  */
@@ -19,55 +14,42 @@ if ( post_password_required() )
 	return;
 ?>
 
-	<div id="comments" class="comments-area">
+	<div id="comments" class="clearfix">
+		<?php if ( have_comments() ) : ?>
 
-	<?php // You can start editing here -- including this comment! ?>
+		<h3>Comments</h3>
+		<div id="comments-list">
+			<ol class="comment-list">
+				<?php
+					$args = array(
+						'type'        => 'comment',
+						'avatar_size' => 48,
+						'callback'    => 'edd_comment',
+					);
 
-	<?php if ( have_comments() ) : ?>
-		<h2 class="comments-title">
-			<?php
-				printf( _nx( 'One thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', get_comments_number(), 'comments title', 'edd' ),
-					number_format_i18n( get_comments_number() ), '<span>' . get_the_title() . '</span>' );
-			?>
-		</h2>
+					wp_list_comments( $args );
+				?>
+			</ol><!-- /.comment-list -->
+		</div><!-- /#comments-list -->
+		
+		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : ?>
+		<div id="comment-navigation">
+			<nav class="navigation comment-nav" role="navigation">
+				<?php previous_comments_link( '<i class="icon-chevron-left"></i>' . __( 'Previous Comments', 'flat' ) ); ?>
+				<?php next_comments_link( __( 'Newer Comments', 'flat' ) . '<i class="icon-chevron-right"></i>'); ?>
+			</nav><!-- /.comment-nav -->
+		</div><!-- /#comment-naviation -->
+		<?php endif; ?>
+		
+		<?php
+		endif; // have_comments()
+		
+		if ( ! comments_open() && '0' != get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) {
+		?>
+		<p class="no-comments">Comments are closed.</p>
+		<?php
+		}
+		?>
+	</div><!-- /#comments -->
 
-		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // are there comments to navigate through ?>
-		<nav id="comment-nav-above" class="navigation-comment" role="navigation">
-			<h1 class="screen-reader-text"><?php _e( 'Comment navigation', 'edd' ); ?></h1>
-			<div class="nav-previous"><?php previous_comments_link( __( '&larr; Older Comments', 'edd' ) ); ?></div>
-			<div class="nav-next"><?php next_comments_link( __( 'Newer Comments &rarr;', 'edd' ) ); ?></div>
-		</nav><!-- #comment-nav-above -->
-		<?php endif; // check for comment navigation ?>
-
-		<ol class="comment-list">
-			<?php
-				/* Loop through and list the comments. Tell wp_list_comments()
-				 * to use edd_comment() to format the comments.
-				 * If you want to overload this in a child theme then you can
-				 * define edd_comment() and that will be used instead.
-				 * See edd_comment() in inc/template-tags.php for more.
-				 */
-				wp_list_comments( array( 'callback' => 'edd_comment' ) );
-			?>
-		</ol><!-- .comment-list -->
-
-		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // are there comments to navigate through ?>
-		<nav id="comment-nav-below" class="navigation-comment" role="navigation">
-			<h1 class="screen-reader-text"><?php _e( 'Comment navigation', 'edd' ); ?></h1>
-			<div class="nav-previous"><?php previous_comments_link( __( '&larr; Older Comments', 'edd' ) ); ?></div>
-			<div class="nav-next"><?php next_comments_link( __( 'Newer Comments &rarr;', 'edd' ) ); ?></div>
-		</nav><!-- #comment-nav-below -->
-		<?php endif; // check for comment navigation ?>
-
-	<?php endif; // have_comments() ?>
-
-	<?php
-		// If comments are closed and there are comments, let's leave a little note, shall we?
-		if ( ! comments_open() && '0' != get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) :
-	?>
-		<p class="no-comments"><?php _e( 'Comments are closed.', 'edd' ); ?></p>
-	<?php endif; ?>
-
-	<?php comment_form(); ?>
-
-</div><!-- #comments -->
+	<?php eddwp_comment_form(); ?>
