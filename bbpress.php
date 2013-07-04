430259075ac9c02acd3c76fb042302a305bdad66
Sunny Ratilal <sunnyratilal5@gmail.com>
2013-07-03 20:18:43 -0500

    Only display the topic meta on the single topic view and make sure the moderators see the admin sidebar.

--- bbpress.php
+++ bbpress.php
@@ -11,6 +11,7 @@ get_header(); ?>
 		<div class="container clearfix">
 			<section class="content">
 				<?php while ( have_posts() ) { the_post(); ?>
+				<?php if ( bbp_is_single_view() ) { ?>
 				<div class="bbp-discussion-meta clearfix">
 					<div class="left">
 						<a href="<?php echo get_permalink( bbp_get_global_post_field( 'post_parent', 'display', bbp_get_topic_id() ) ); ?>">&larr; Back to <?php echo get_the_title( bbp_get_global_post_field( 'post_parent', 'display', bbp_get_topic_id() ) ); ?></a>
@@ -22,11 +23,22 @@ get_header(); ?>
 						<?php } // end if ?>
 					</div>
 				</div>
+				<?php } // end if ?>
 				<h1><?php the_title(); ?></h1>
 				<?php the_content(); ?>
 				<?php } ?>
 			</section><!-- /.content -->
-			<?php get_sidebar( 'forums' ); ?>
+			<?php
+			
+			if ( ! current_user_can( 'moderate' ) ) {
+				get_sidebar( 'forums' );
+			} else {
+				echo '<aside class="sidebar">';
+				edd_bbp_d_sidebar();
+				echo '</aside><!-- /.sidebar -->';
+			}
+			
+			?>
 		</div><!-- /.container -->
 	</section><!-- /.main -->
 
