9c4f2a58c13006fb9338b8b7261ca020151f4b97
Sunny Ratilal <sunnyratilal5@gmail.com>
2013-07-03 20:25:50 -0500

    Format the footer template.

--- footer.php
+++ footer.php
@@ -2,9 +2,9 @@
 /**
  * The template for displaying the footer.
  *
- * Contains the closing of the id=main div and all content after
- *
  * @package EDD
+ * @version 1.0
+ * @since   1.0
  */
 ?>
 
@@ -14,32 +14,30 @@
 			<div class="columns clearfix">
 				<div class="dev-blog col">
 					<?php eddwp_get_latest_post(); ?>
-				</div>
+				</div><!-- /.dev-blog -->
 
 				<div class="forum col">
 					<h4>Need help?</h4>
 					<p>If you ever need help with EDD, there is a complete <a href="">Support Forum</a> available where you can get your support questions answered. If you'd like to report a bug or have ideas for how to improve the plugin, please post it to our <a href="">GitHub Issue Tracker</a>.</p>
-				</div>
+				</div><!-- /.forum -->
 
 				<div class="consultants col">
 					<h4>Trusted Consultants</h4>
 					<p>We maintain a list of consultants that we recommend working with when it comes to managed support, customization, and setup help. </p>
 					<p><a href="">View Consultants...</a></p>
-				</div>
-			</div>
+				</div><!-- /.consultants -->
+			</div><!-- /.columns -->
 
 			<div class="social clearfix">
 				<ul>
-					<li class="facebook"><a href="#"></a></li>
-					<li class="twitter"><a href="#"></a></li>
-					<li class="gplus"><a href="#"></a></li>
-					<li class="github"><a href="#"></a></li>
+					<li class="facebook"><a href="https://www.facebook.com/eddwp"></a></li>
+					<li class="twitter"><a href="https://twitter.com/eddwp"></a></li>
+					<li class="gplus"><a href="https://plus.google.com/111409933861760783237/posts"></a></li>
+					<li class="github"><a href="https://github.com/easydigitaldownloads/Easy-Digital-Downloads/"></a></li>
 				</ul>
-			</div>
+			</div><!-- /.social -->
 
-			<?php
-			eddwp_get_footer_nav();
-			?>
+			<?php eddwp_get_footer_nav(); ?>
 			<p class="copyright">Copyright &copy; 2013, Easy Digital Downloads. A project by <a href="<?php echo esc_url( '/team/' ); ?>">Pippin Williamson and Friends</a>.</p>
 		</div><!-- .container -->
 	</footer><!-- #colophon -->
