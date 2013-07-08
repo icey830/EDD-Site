89762dfffc5560a31433219c799a90d3889d93e9
Sunny Ratilal <sunnyratilal5@gmail.com>
2013-07-08 13:23:31 -0500

    Add 404 template.

--- 404.php
+++ 404.php
@@ -9,50 +9,41 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<div id="content" class="site-content" role="main">
-
-			<article id="post-0" class="post not-found">
-				<header class="entry-header">
-					<h1 class="entry-title"><?php _e( 'Oops! That page can&rsquo;t be found.', 'edd' ); ?></h1>
-				</header><!-- .entry-header -->
-
-				<div class="entry-content">
-					<p><?php _e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'edd' ); ?></p>
-
+	<section class="main clearfix">
+		<div class="container clearfix">
+			<section class="content">
+				<article class="post" id="post-0">
+					<h1>Oops! That page can&rsquo;t be found.</h1>
+					<p>It looks like nothing was found at this location. Maybe try one of the links below or a search?</p>
+					
 					<?php get_search_form(); ?>
-
+					
 					<?php the_widget( 'WP_Widget_Recent_Posts' ); ?>
-
-					<?php if ( edd_categorized_blog() ) : // Only show the widget if site has multiple categories. ?>
-					<div class="widget widget_categories">
-						<h2 class="widgettitle"><?php _e( 'Most Used Categories', 'edd' ); ?></h2>
-						<ul>
-						<?php
-							wp_list_categories( array(
-								'orderby'    => 'count',
-								'order'      => 'DESC',
-								'show_count' => 1,
-								'title_li'   => '',
-								'number'     => 10,
-							) );
-						?>
-						</ul>
-					</div><!-- .widget -->
-					<?php endif; ?>
-
-					<?php
-					/* translators: %1$s: smiley */
-					$archive_content = '<p>' . sprintf( __( 'Try looking in the monthly archives. %1$s', 'edd' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
-					?>
-
-					<?php the_widget( 'WP_Widget_Tag_Cloud' ); ?>
-
-				</div><!-- .entry-content -->
-			</article><!-- #post-0 .post .not-found -->
-
-		</div><!-- #content -->
-	</div><!-- #primary -->
+				</article>
+			</section><!-- /.content -->
+			
+			<aside class="sidebar">
+				<div class="newsletter">
+					<h3>Email Newsletter</h3>
+					<p>Sign up to receive regular updates from Easy Digital Downloads.</p>
+					<form id="pmc_mailchimp" action="" method="post">
+						<div>
+							<input name="pmc_fname" id="pmc_fname" type="text" placeholder="<?php _e('Name'); ?>"/>
+						</div>
+						<div>
+							<input name="pmc_email" id="pmc_email" type="text" placeholder="<?php _e('Enter your email address'); ?>"/>
+						</div>
+						<div>
+							<input type="hidden" name="redirect" value="<?php echo 'https://' . $_SERVER["SERVER_NAME"] . $_SERVER["REQUEST_URI"]; ?>"/>
+							<input type="hidden" name="action" value="pmc_signup"/>
+							<input type="hidden" name="pmc_list_id" value="<?php echo $list_id; ?>"/>
+							<input type="submit" value="<?php _e( 'Sign Up' ); ?>"/>
+						</div>
+					</form>
+				</div>
+				<?php dynamic_sidebar( 'blog-sidebar' ); ?>
+			</aside><!-- /.sidebar -->
+		</div><!-- /.container -->
+	</section><!-- /.main -->
 
 <?php get_footer(); ?>
