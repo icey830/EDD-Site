269206e882761f32b6b6feb94be7e543ba2168f5
Sunny Ratilal <sunnyratilal5@gmail.com>
2013-07-10 08:39:12 -0500

    Add security badge to the checkout

--- functions.php
+++ functions.php
@@ -562,3 +562,13 @@ function eddwp_shortcode_toggle( $atts, $content = null ) {
     return $output;
 }
 add_shortcode( 'toggle', 'eddwp_shortcode_toggle' );
+
+/**
+ * Add Security Info to the Checkout
+ */
+function eddwp_add_security_info() {
+	?>
+	<a href="https://www.PositiveSSL.com" id="ssl-seal" title="SSL Certificate Authority" style="font-family: arial; font-size: 10px; text-decoration: none;"><img src="https://www.positivessl.com/images/seals/PositiveSSL_tl_trans.gif" alt="SSL Certificate Authority" title="SSL Certificate Authority" border="0"><br>SSL Certificate Authority</a>
+	<?php
+}
+add_action( 'edd_cc_form', 'eddwp_add_security_info' );
