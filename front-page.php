4e6c8da7b7c0feb5912cebc6742f9b07b38da542
Sunny Ratilal <sunnyratilal5@gmail.com>
2013-07-17 05:35:03 -0500

    Comment out the video containers.

--- front-page.php
+++ front-page.php
@@ -20,17 +20,18 @@
 			<div class="hero-text">
 				<h1>Say hello to the world's easiest way to sell digital downloads through WordPress</h1>
 				<h2>for free</h2>
-				<p><a href="http://downloads.wordpress.org/plugin/easy-digital-downloads.latest-stable.zip" class="download-button"><i class="icon-cloud-download"></i> Download</a> <a href="https://easydigitaldownloads.com/demo/" target="_blank" class="download-button"><i class="icon-eye-open"></i> Demo</a></p>
+				<p><a href="http://downloads.wordpress.org/plugin/easy-digital-downloads.latest-stable.zip" class="download-button"><i class="icon-cloud-download"></i> Download</a> <a href="https://easydigitaldownloads.com/demo/" target="_blank" class="download-button demo-button"><i class="icon-eye-open"></i> Demo</a></p>
 			</div><!-- /.hero-text -->
 			<img src="<?php echo get_template_directory_uri(); ?>/images/edd-standing.png" />
 		</div><!-- .hero-inside -->
-		<div id="video-container"></div><!-- #video-container -->
-		<div id="video-complete">
+		<!-- <div id="video-container"></div> --><!-- #video-container -->
+		<!--
+<div id="video-complete">
 			<div id="video-complete-inner">
 					<a class="replay-button"><span class="button"></span> Replay</a>
 				<a class="close-button"><span class="button"></span> Close</a>
 			</div><!-- #video-complete-inner -->
-		</div><!-- /#video-complete -->
+		<!--</div>--><!-- /#video-complete -->
 	</section><!-- /.hero -->
 </div><!-- /.hero-container -->
 
