30a5ec3ce6316df628ef5658e79f085a4844cf9e
Pippin Williamson <pippin@pippinsplugins.com>
2013-12-31 11:17:26 -0600

    Fix pagination of author archives

--- author.php
+++ author.php
@@ -13,7 +13,7 @@
 ?>
 
 <?php
-global $post;
+global $post, $wp_query;
 get_header();
 ?>
 
@@ -51,7 +51,7 @@ get_header();
 						'base'    => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
 						'format'  => '?paged=%#%',
 						'current' => max( 1, get_query_var('paged') ),
-						'total'   => $extensions->max_num_pages
+						'total'   => $wp_query->max_num_pages
 					)
 				);
 				?>
