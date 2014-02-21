39fa70bca50d47bf8f1fba76cd8cc84a05c469db
Sunny Ratilal <sunnyratilal5@gmail.com>
2014-02-21 13:45:49 +0000

    Rework pagination a little and remove redundant, repeated code. See #222.

--- archive-videos.php
+++ archive-videos.php
@@ -32,16 +32,7 @@ get_header();
 					<?php
 				endwhile;
 
-				$big = 999999999;
-
-				$links = paginate_links(
-					array(
-						'base'    => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
-						'format'  => '?paged=%#%',
-						'current' => max( 1, get_query_var('paged') ),
-						'total'   => $wp_query->max_num_pages
-					)
-				);
+				eddwp_paginate_links();
 				?>
 				<div class="clear"></div>
 				<div class="pagination">
--- functions.php
+++ functions.php
@@ -401,12 +401,11 @@ function eddwp_paginate_links() {
 
 	$big = 999999999;
 
-	echo '<div id="page-nav">' . paginate_links( array(
+	echo '<div class="pagination">' . paginate_links( array(
 		'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
 		'format' => '?paged=%#%',
 		'current' => max( 1, get_query_var( 'paged' ) ),
 		'total' => $wp_query->max_num_pages,
-		'type' => 'list'
 	) ) . '</div>';
 }
 
