48cb5e5ff5b2e5023a3804e8dd7e6446c92900f2
sunnyratilal <sunnyratilal5@gmail.com>
2014-08-06 10:14:17 +0100

    Add testimonials to features page. See #248

--- template-features.php
+++ template-features.php
@@ -47,4 +47,39 @@ the_post();
 		</section><!-- /.feature-text -->
 	</section><!-- /.feature-area -->
 
+	<section class="feature-area" id="feature-testimonials">
+		<section class="feature-text">
+			<div class="feature-text-inner">
+				<h2>With over 300,000 downloads, we got your back...</h2>
+				<p>We have <del>some</del> <ins>a lot</ins> of happy customers and here's what some are saying:</p>
+
+				<div class="testimonials">
+				<?php
+				$testimonials = new WP_Query(
+					array(
+						'posts_per_page' => 2,
+						'post_type' => 'testimonials',
+						'orderby' => 'rand',
+						'post_status' => 'publish',
+					)
+				);
+
+				while ( $testimonials->have_posts() ) {
+					$testimonials->the_post(); ?>
+					<div class="quote">
+						<blockquote>
+							<p><?php the_content(); ?></p>
+							<cite><?php echo get_post_meta( get_the_ID(), 'ecpt_author', true ); ?></cite>
+						</blockquote>
+					</div>
+				<?php
+				}
+
+				wp_reset_postdata();
+				?>
+				</div><!-- /.testimonials -->
+			</div><!-- /.feature-text-inner -->
+		</section><!-- /.feature-text -->
+	</section><!-- /.feature-area -->
+
 <?php get_footer(); ?>
