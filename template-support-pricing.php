26bea59591b872106bba5a524e0c3dd2a571b2c2
Pippin Williamson <pippin@pippinsplugins.com>
2014-08-11 15:04:20 -0500

    Query testimonials instead of hard coding the HTML. #260

--- template-support-pricing.php
+++ template-support-pricing.php
@@ -21,4 +21,36 @@ get_header();
 
 the_content();
 
+?>
+<section class="main clearfix">
+	<section class="content clearfix">
+		<h2>We have a <ins>lot</ins> of happy customers!</h2>
+
+		<?php
+		$testimonials = new WP_Query(
+			array(
+				'posts_per_page' => 6,
+				'post_type' => 'testimonials',
+				'orderby' => 'rand',
+				'post_status' => 'publish',
+			)
+		);
+
+		while ( $testimonials->have_posts() ) {
+			$testimonials->the_post(); ?>
+			<div class="quote">
+				<blockquote>
+					<p><?php the_content(); ?></p>
+					<cite><?php echo get_post_meta( get_the_ID(), 'ecpt_author', true ); ?></cite>
+				</blockquote>
+			</div>
+		<?php
+		}
+
+		wp_reset_postdata();
+		?>
+	</section><!-- /.content -->
+</section><!-- /.main -->
+<?php
+
 get_footer();
