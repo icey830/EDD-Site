1683d1dc329b3565bec42d6111bf3bc03604db9b
Sean Davis <sean@sdavismedia.com>
2015-01-29 15:48:38 -0600

    clear pagination div

--- functions.php
+++ functions.php
@@ -409,7 +409,7 @@ function eddwp_paginate_links() {
 
 	$big = 999999999;
 
-	echo '<div class="pagination">' . paginate_links( array(
+	echo '<div class="pagination clear">' . paginate_links( array(
 		'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
 		'format' => '?paged=%#%',
 		'current' => max( 1, get_query_var( 'paged' ) ),
