5724e12150a92cb6d5c1dcb4fe3f8bd540c7ff83
Sean Davis <sean@sdavismedia.com>
2015-03-21 15:49:29 -0500

    update home template with newsletter function

--- home.php
+++ home.php
@@ -66,25 +66,7 @@
 			</section><!-- /.content -->
 
 			<aside class="sidebar">
-				<div class="newsletter widget">
-					<h3 class="newsletter-title">Email Newsletter</h3>
-					<p class="newsletter-description">Sign up to the Easy Digital Downloads email newsletter and be the first to know about the latest information and exclusive promotions.</p>
-					<form class="newsletter-form" id="pmc_mailchimp" action="" method="post">
-						<div>
-							<input class="newsletter-name" name="pmc_fname" id="pmc_fname" type="text" placeholder="First Name"/>
-						</div>
-						<div>
-							<input class="newsletter-email" name="pmc_email" id="pmc_email" type="text" placeholder="Email Address"/>
-						</div>
-						<div>
-							<input type="hidden" name="redirect" value="<?php echo 'https://' . $_SERVER["SERVER_NAME"] . $_SERVER["REQUEST_URI"]; ?>"/>
-							<input type="hidden" name="action" value="pmc_signup"/>
-							<input type="hidden" name="pmc_list_id" value="<?php echo $list_id; ?>"/>
-							<input type="submit" value="Sign Up"/>
-						</div>
-					</form>
-					<p class="newsletter-note"><i class="fa fa-lock"></i>We will never send you spam. Your email address is secure.</p>
-				</div>
+				<?php eddwp_newsletter_sidebar(); ?>
 				<?php dynamic_sidebar( 'blog-sidebar' ); ?>
 			</aside><!-- /.sidebar -->
 		</div><!-- /.container -->
