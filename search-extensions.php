ada6339ed4a9df830341718267927c90943f1db3
Sean Davis <sean@sdavismedia.com>
2015-03-21 18:59:10 -0500

    revert extensions search results pagination commit

--- search-extensions.php
+++ search-extensions.php
@@ -56,52 +56,66 @@
 		<section class="extensions-container">
 			<div class="extensions clearfix">
 				<?php
-					$tax_query = array(
-						array(
-							'taxonomy' => 'extension_category',
-							'field' => 'slug',
-							'terms' => $q['c']
-						)
-					);
-	
-					$query = array(
-						's' => $q['s'],
-						'post_type' => 'extension',
-						'posts_per_page' => 21,
-						'paged' => isset( $_GET['page'] ) ? (int) $_GET['page'] : 1
-					);
-					if( $q['c'] ) {
-						$query['tax_query'] = $tax_query;
-					}
-	
-					$s_query = new WP_Query( $query );
-	
-					$query = $s_query;
-	
-					$c = 0; while ( $query->have_posts() ) { $query->the_post(); $c++; ?>
-						<div class="extension <?php if ( 0 == $c%3 ) echo ' extension-clear'; ?>">
-							<a href="<?php echo home_url( '/extensions/' . $post->post_name ); ?>" title="<?php get_the_title(); ?>">
-								<div class="thumbnail-holder"><?php the_post_thumbnail( 'showcase' ); ?></div>
-								<h2><?php the_title(); ?></h2>
-								<?php echo get_post_meta( get_the_ID(), 'ecpt_shortdescription', true ); ?>
-							</a>
-							<div class="overlay">
-								<a href="<?php echo home_url( '/extensions/' . $post->post_name ); ?>" class="overlay-view-details button">View Details</a>
-								<?php if( ! eddwp_is_external_extension() ) : ?>
-									<a href="<?php echo home_url( '/edd-add/' . get_post_meta( get_the_ID(), 'ecpt_downloadid', true ) ); ?>" class="overlay-add-to-cart button">Add to Cart</a>
-								<?php endif; ?>
-							</div>
-							<?php
-							if ( has_term( '3rd Party', 'extension_category', get_the_ID() ) ) {
-								echo '<i class="third-party"></i>';
-							}
-							?>
+
+				$tax_query = array(
+					array(
+						'taxonomy' => 'extension_category',
+						'field' => 'slug',
+						'terms' => $q['c']
+					)
+				);
+
+				$query = array(
+					's' => $q['s'],
+					'post_type' => 'extension',
+					'posts_per_page' => 21,
+					'paged' => isset( $_GET['page'] ) ? (int) $_GET['page'] : 1
+				);
+				if( $q['c'] ) {
+					$query['tax_query'] = $tax_query;
+				}
+
+				$s_query = new WP_Query( $query );
+
+				$query = $s_query;
+
+				$c = 0; while ( $query->have_posts() ) { $query->the_post(); $c++; ?>
+					<div class="extension <?php if ( 0 == $c%3 ) echo ' extension-clear'; ?>">
+						<a href="<?php echo home_url( '/extensions/' . $post->post_name ); ?>" title="<?php get_the_title(); ?>">
+							<div class="thumbnail-holder"><?php the_post_thumbnail( 'showcase' ); ?></div>
+							<h2><?php the_title(); ?></h2>
+							<?php echo get_post_meta( get_the_ID(), 'ecpt_shortdescription', true ); ?>
+						</a>
+						<div class="overlay">
+							<a href="<?php echo home_url( '/extensions/' . $post->post_name ); ?>" class="overlay-view-details button">View Details</a>
+							<?php if( ! eddwp_is_external_extension() ) : ?>
+								<a href="<?php echo home_url( '/edd-add/' . get_post_meta( get_the_ID(), 'ecpt_downloadid', true ) ); ?>" class="overlay-add-to-cart button">Add to Cart</a>
+							<?php endif; ?>
 						</div>
 						<?php
-					}
-					eddwp_paginate_links();
-					wp_reset_postdata();
+						if ( has_term( '3rd Party', 'extension_category', get_the_ID() ) ) {
+							echo '<i class="third-party"></i>';
+						}
+						?>
+					</div>
+				<?php
+				}
+
+				$big = 999999999;
+				$base = home_url( 'extensions' ) . '/?' . remove_query_arg( 'page', $_SERVER['QUERY_STRING'] ) . '%_%';
+
+				$links = paginate_links( array(
+					'base' => $base,
+					'format' => '&page=%#%',
+					'current' => max( 1, isset( $_GET['page'] ) ? (int) $_GET['page'] : 1 ),
+					'total' => $query->max_num_pages
+				));
 				?>
+				<div class="clear"></div>
+				<div class="pagination">
+					<?php echo $links; ?>
+				</div>
+				<?php wp_reset_postdata(); ?>
 			</div><!-- /.extensions -->
 		</section><!-- /.extensions-container -->
 	</section><!-- /.main -->
