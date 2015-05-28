5d5099f99da28f1049a365c799900e5523049042
Sean Davis <sean@sdavismedia.com>
2015-05-28 00:32:26 -0500

    adjust empty cart to pull downloads instead of extensions and themes

--- page-checkout.php
+++ page-checkout.php
@@ -24,10 +24,105 @@ get_header(); ?>
 						<article class="entry">
 							<h1>Your cart is empty!</h1>
 							<h2 class="tagline">Here are some extensions you may be interested in.</h2>
-							<?php eddwp_display_extensions(); ?>
-
+							<section class="download-grid two-col clearfix">
+								<?php
+									$eddwp_ext_args = array(
+										'post_type'      => 'download',
+										'posts_per_page' => 2,
+										'tax_query'      => array(
+											'relation'     => 'AND',
+											array(
+												'taxonomy' => 'download_category',
+												'field'    => 'slug',
+												'terms'    => 'extensions',
+											),
+											array(
+												'taxonomy' => 'download_category',
+												'field'    => 'slug',
+												'terms'    => 'recommended',
+											),
+											array(
+												'taxonomy' => 'download_category',
+												'field'    => 'slug',
+												'terms'    => '3rd-party',
+												'operator' => 'NOT IN',
+											),
+										),
+									);
+									$eddwp_ext = new WP_Query( $eddwp_ext_args );
+		
+									while ( $eddwp_ext->have_posts() ) : $eddwp_ext->the_post();
+										?>
+										<div class="download-grid-item">
+											<a href="<?php echo home_url( '/downloads/' . $post->post_name ); ?>" title="<?php get_the_title(); ?>">
+												<?php eddwp_downloads_grid_thumbnail(); ?>
+											</a>
+											<div class="download-grid-item-info">
+												<?php
+													the_title( '<h4 class="download-grid-title">', '</h4>' );
+													$short_desc = get_post_meta( get_the_ID(), 'ecpt_shortdescription', true );
+													echo $short_desc;
+												?>
+											</div>
+											<div class="download-grid-item-cta">
+												<a class="download-grid-item-primary-link button" href="<?php echo home_url( '/downloads/' . $post->post_name ); ?>" title="<?php get_the_title(); ?>">More Information</a>
+											</div>
+										</div>
+										<?php
+									endwhile;
+									wp_reset_postdata();
+								?>
+							</section><!-- .download-grid two-col -->
 							<h2 class="tagline">Or maybe you're interested in some themes?</h2>
-							<?php eddwp_display_themes(); ?>
+							<section class="download-grid two-col clearfix">
+								<?php
+									$eddwp_theme_args = array(
+										'post_type'      => 'download',
+										'posts_per_page' => 2,
+										'tax_query'      => array(
+											'relation'     => 'AND',
+											array(
+												'taxonomy' => 'download_category',
+												'field'    => 'slug',
+												'terms'    => 'themes',
+											),
+											array(
+												'taxonomy' => 'download_category',
+												'field'    => 'slug',
+												'terms'    => 'recommended',
+											),
+											array(
+												'taxonomy' => 'download_category',
+												'field'    => 'slug',
+												'terms'    => '3rd-party',
+												'operator' => 'NOT IN',
+											),
+										),
+									);
+									$eddwp_themes = new WP_Query( $eddwp_theme_args );
+		
+									while ( $eddwp_themes->have_posts() ) : $eddwp_themes->the_post();
+										?>
+										<div class="download-grid-item">
+											<a href="<?php echo home_url( '/downloads/' . $post->post_name ); ?>" title="<?php get_the_title(); ?>">
+												<?php eddwp_downloads_grid_thumbnail(); ?>
+											</a>
+											<div class="download-grid-item-info">
+												<?php
+													the_title( '<h4 class="download-grid-title">', '</h4>' );
+													$short_desc = get_post_meta( get_the_ID(), 'ecpt_shortdescription', true );
+													echo $short_desc;
+												?>
+											</div>
+											<div class="download-grid-item-cta">
+												<a class="download-grid-item-primary-link button" href="<?php echo home_url( '/downloads/' . $post->post_name ); ?>" title="<?php get_the_title(); ?>">More Information</a>
+											</div>
+										</div>
+										<?php
+									endwhile;
+									wp_reset_postdata();
+								?>
+							</section><!-- .download-grid two-col -->
 						</article>
 					<?php } ?>
 				<?php } ?>
