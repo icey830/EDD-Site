752c23c2136ba3519cf839c5ec1d38ffd5a21f29
Sean Davis <sean@sdavismedia.com>
2015-07-01 11:39:39 -0500

    update video archive display

--- archive-videos.php
+++ archive-videos.php
@@ -13,33 +13,39 @@ get_header();
 ?>
 	<section class="main clearfix">
 
-		<section class="extensions-container">
-			<h1>Videos</h1>
-			<div class="extensions clearfix">
-				<?php
-				$c = 0; while ( have_posts() ) : the_post(); $c++;
-				?>
-					<div class="extension <?php if ( 0 == $c%3 ) echo ' extension-clear'; ?>">
-						<a href="<?php the_permalink(); ?>" title="<?php get_the_title(); ?>">
-							<div class="thumbnail-holder"><?php the_post_thumbnail( 'showcase' ); ?></div>
-							<h2><?php the_title(); ?></h2>
-						</a>
-						<?php the_excerpt(); ?>
-						<div class="overlay">
-							<a href="<?php the_permalink(); ?>" class="overlay-view-details button">View Video</a>
-						</div>
+		<div class="extensions-header-area full-width">
+			<div class="inner">
+				<div class="extensions-header clearfix">
+					<div class="section-header">
+						<h2 class="section-title">Easy Digital Downloads Videos</h2>
 					</div>
-					<?php
-				endwhile;
-
-				eddwp_paginate_links();
-				?>
-				<div class="clear"></div>
-				<div class="pagination">
-					<?php echo $links; ?>
 				</div>
-				<?php wp_reset_postdata(); ?>
 			</div>
-		</section><!-- /.extensions-container -->
+		</div>
+
+		<div class="edd-downloads-area full-width">
+			<div class="inner">
+				<div class="edd-downloads">
+
+					<section class="download-grid three-col clearfix">
+						<div class="download-grid-item">
+							<?php while ( have_posts() ) : the_post(); ?>
+								<a href="<?php echo home_url( '/videos/' . $post->post_name ); ?>" title="<?php get_the_title(); ?>">
+									<?php eddwp_downloads_grid_thumbnail(); ?>
+								</a>
+								<div class="download-grid-item-info">
+									<?php
+										the_title( '<h4 class="download-grid-title">', '</h4>' );
+										$short_desc = the_excerpt();
+										echo $short_desc;
+									?>
+								</div>
+							<?php endwhile; wp_reset_postdata(); ?>
+						</div>
+					</section><!-- /.extensions-container -->
+
+				</div>
+			</div>
+		</div>
 	</section><!-- /.main -->
 <?php get_footer(); ?>
