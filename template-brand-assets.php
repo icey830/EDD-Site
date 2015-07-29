31b00c0013376ca3b607c36e75b6cfacd9e56b8f
Andrew Munro <andrew@sumobi.com>
2015-07-29 21:51:59 +1200

    Add new brand assets page template

---
+++ template-brand-assets.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Template Name: Brand Assets
+ */
+get_header();
+the_post();
+?>
+	<section class="main clearfix">
+
+        <div class="container clearfix">
+			<section class="content">
+				<article class="entry">
+					<h1>Brand Assets</h1>
+
+                    <p>Thanks for your interest in Easy Digital Downloads!</p>
+                    <p>Below are a few guidelines for using AffiliateWP’s brand resources, please take a moment to familiarize yourself with them. <br/>You can download individual assets in each section, or you can download everything all at once below.</p>
+
+                    <a href="#" class="button">Download all assets</a>
+                    <h2>Colors</h2>
+                    <p>Colors will go here</p>
+
+                    <h2>Logos</h2>
+                    <p>Various logos will go here</p>
+
+                    <h2>Ed is our mascot</h2>
+                    <p>Various mascot images</p>
+
+                    <h2>Screenshots</h2>
+                    <p>A link to the <a href="https://easydigitaldownloads.com/screenshots/">screenshots</a>.</p>
+                </article>
+			</section>
+		</div>
+
+	</section>
+	<?php
+get_footer();
