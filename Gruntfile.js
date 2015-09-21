65a2b5c1e5be86208d002dd1970723ddcb39f593
Sean Davis <sdavis2702@gmail.com>
2015-09-21 00:58:16 -0500

    adjust grunt file formatting

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,105 +1,106 @@
 module.exports = function(grunt) {
 	grunt.initConfig({
 
-	// less
-	less: {
-		development: {
-		options: {
-			compress: true,
-			yuicompress: true,
-			optimization: 2
+		// less
+		less: {
+			development: {
+				options: {
+					compress: true,
+					yuicompress: true,
+					optimization: 2
+				},
+				files: {
+					"style.css": "css/less/style.less"
+				}
+			}
 		},
-		files: {
-			"style.css": "css/less/style.less"
-		}
-		}
-	},
+		
+		// concat
+		concat: {
+			js: {
+				options: {
+					separator: ';'
+				},
+				src: ['js/src/**/*.js', 'js/dev/theme.js'],
+				dest: 'js/theme.min.js'
+			},
+		},
 	
-	// concat
-	concat: {
-		js: {
-		options: {
-			separator: ';'
+		// uglify
+		uglify: {
+			options: {
+				mangle: false
+			},
+			js: {
+				files: {
+					'js/theme.min.js': ['js/theme.min.js']
+				}
+			}
 		},
-		src: ['js/src/**/*.js', 'js/dev/theme.js'],
-		dest: 'js/theme.min.js'
-		},
-	},
-
-	// uglify
-	uglify: {
-		options: {
-		mangle: false
-		},
-		js: {
-		files: {
-			'js/theme.min.js': ['js/theme.min.js']
-		}
-		}
-	},
-
-	// svg store
-	svgstore: {
-		options: {
-		prefix : 'icon-', // This will prefix each <g> ID
-		svg : {
-			'xmlns:sketch' : 'http://www.bohemiancoding.com/sketch/ns',
-			'xmlns:dc': "http://purl.org/dc/elements/1.1/",
-			'xmlns:cc': "http://creativecommons.org/ns#",
-			'xmlns:rdf': "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
-			'xmlns:svg': "http://www.w3.org/2000/svg",
-			'xmlns': "http://www.w3.org/2000/svg",
-			'xmlns:sodipodi': "http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd",
-			'xmlns:inkscape': "http://www.inkscape.org/namespaces/inkscape"
-		}
-		},
-		default : {
-		files: {
-			'images/svg-defs.svg': ['svgs/*.svg'],
-		}
-		}
-	},
-
-	watch: {
-		styles: {
-		files: ['css/less/*.less'],
-		tasks: ['less'],
-		options: {
-			nospawn: true
-		}
-		},
-		js: {
-		files: ['js/src/**/*.js'],
-		tasks: ['concat:js', 'uglify:js'],
-		},
+	
+		// svg store
 		svgstore: {
-		files: ['svgs/*.svg'],
-		tasks: ['svgstore:default']
+			options: {
+				prefix : 'icon-', // This will prefix each <g> ID
+				svg : {
+					'xmlns:sketch' : 'http://www.bohemiancoding.com/sketch/ns',
+					'xmlns:dc': "http://purl.org/dc/elements/1.1/",
+					'xmlns:cc': "http://creativecommons.org/ns#",
+					'xmlns:rdf': "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
+					'xmlns:svg': "http://www.w3.org/2000/svg",
+					'xmlns': "http://www.w3.org/2000/svg",
+					'xmlns:sodipodi': "http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd",
+					'xmlns:inkscape': "http://www.inkscape.org/namespaces/inkscape"
+				}
+			},
+			default : {
+				files: {
+					'images/svg-defs.svg': ['svgs/*.svg'],
+				}
+			}
+		},
+	
+		watch: {
+			styles: {
+				files: ['css/less/*.less'],
+				tasks: ['less'],
+				options: {
+					nospawn: true
+				}
+			},
+			js: {
+				files: ['js/src/**/*.js'],
+				tasks: ['concat:js', 'uglify:js'],
+			},
+			svgstore: {
+				files: ['svgs/*.svg'],
+				tasks: ['svgstore:default']
+			}
+		},
+	
+		'sftp-deploy': {
+			build: {
+			auth: {
+				host: 'edd.staging.wpengine.com',
+				port: 22,
+				authKey: 'staging'
+			},
+			src: '.',
+			dest: '/wp-content/themes/edd-v2/',
+			exclusions: ['.DS_Store', './git']
+			},
+			css: {
+				auth: {
+					host: 'edd.staging.wpengine.com',
+					port: 22,
+					authKey: 'staging'
+				},
+				src: '.',
+				dest: '/wp-content/themes/edd-v2/',
+				exclusions: ['.DS_Store', './git']
+			}
 		}
-	},
 
-	'sftp-deploy': {
-		build: {
-		auth: {
-			host: 'edd.staging.wpengine.com',
-			port: 22,
-			authKey: 'staging'
-		},
-		src: '.',
-		dest: '/wp-content/themes/edd-v2/',
-		exclusions: ['.DS_Store', './git']
-		},
-		css: {
-		auth: {
-			host: 'edd.staging.wpengine.com',
-			port: 22,
-			authKey: 'staging'
-		},
-		src: '.',
-		dest: '/wp-content/themes/edd-v2/',
-		exclusions: ['.DS_Store', './git']
-		}
-	}
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-less');
@@ -110,6 +111,5 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-svgstore');
 
 	grunt.registerTask('default', ['concat', 'uglify', 'watch', 'sftp-deploy', 'svgstore']);
-
 
 };
