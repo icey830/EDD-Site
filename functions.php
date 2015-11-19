8dcf7d7a5ce62b3b046b00f25b6695805146d66b
Sean Davis <sdavis2702@gmail.com>
2015-11-18 21:51:38 -0800

    bust staging cache

--- functions.php
+++ functions.php
@@ -34,7 +34,7 @@
  * 1. Theme Setup
  * ----------------------------------------------------------- */
 
-define( 'EDD_SITE_VERSION', '2.1.5' );
+define( 'EDD_SITE_VERSION', '2.1.5.1' );
 
 /**
  * Set the content width.
