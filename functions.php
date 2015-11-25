2ef502d36d91a634323e10f2400c5657becc1dd6
Sean Davis <sdavis2702@gmail.com>
2015-11-25 10:36:52 -0800

    version bump

--- functions.php
+++ functions.php
@@ -34,7 +34,7 @@
  * 1. Theme Setup
  * ----------------------------------------------------------- */
 
-define( 'EDD_SITE_VERSION', '2.1.5.1' );
+define( 'EDD_SITE_VERSION', '2.1.6' );
 
 /**
  * Set the content width.
