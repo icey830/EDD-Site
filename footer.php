88898656f0f494524774e341f38e145256e63812
Sean Davis <sdavis2702@gmail.com>
2015-12-08 08:50:53 -0800

    add GoDaddy badge to footer #827

--- footer.php
+++ footer.php
@@ -13,7 +13,7 @@
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="container">
 			<?php if ( ! is_404() ) : ?>
-				<img src="<?php echo get_template_directory_uri(); ?>/images/edd-sitting.png" />
+				<img class="sitting-edd" src="<?php echo get_template_directory_uri(); ?>/images/edd-sitting.png" />
 			<?php endif; ?>
 			<?php if ( ! is_page( array( 'checkout', 130, 635 ) ) ) : ?>
 				<div class="columns clearfix">
@@ -32,20 +32,25 @@
 					</div><!-- /.consultants -->
 				</div><!-- /.columns -->
 
-				<?php if ( has_nav_menu( 'footer' ) ) : ?>
-					<nav id="footer-menu" class="navigation-footer">
-						<?php wp_nav_menu( array( 'theme_location' => 'footer' ) ); ?>
-					</nav><!-- /#primary -->
-				<?php endif; ?>
+				<div class="site-info">
+					<?php if ( has_nav_menu( 'footer' ) ) : ?>
+						<nav id="footer-menu" class="navigation-footer">
+							<?php wp_nav_menu( array( 'theme_location' => 'footer' ) ); ?>
+						</nav><!-- /#primary -->
+					<?php endif; ?>
 
-				<div class="social clearfix">
-					<ul>
-						<li class="facebook"><a href="https://www.facebook.com/eddwp"></a></li>
-						<li class="twitter"><a href="https://twitter.com/eddwp"></a></li>
-						<li class="gplus"><a href="https://plus.google.com/111409933861760783237/posts"></a></li>
-						<li class="github"><a href="https://github.com/easydigitaldownloads/Easy-Digital-Downloads/"></a></li>
-					</ul>
-				</div><!-- /.social -->
+					<div class="social clearfix">
+						<ul>
+							<li class="facebook"><a href="https://www.facebook.com/eddwp"></a></li>
+							<li class="twitter"><a href="https://twitter.com/eddwp"></a></li>
+							<li class="gplus"><a href="https://plus.google.com/111409933861760783237/posts"></a></li>
+							<li class="github"><a href="https://github.com/easydigitaldownloads/Easy-Digital-Downloads/"></a></li>
+						</ul>
+					</div><!-- /.social -->
+
+					<a class="godaddy-partner" href="https://garage.godaddy.com/wordpress-plugin-partner-program/directory/?cvosrc=advocacy.evangelism.WP3" rel="nofollow"><img src="https://garage.godaddy.com/wp-content/uploads/badges/godaddy-plugin-partner-program-badge.svg" title="GoDaddy Plugin Program Partner" alt="GoDaddy Plugin Program Partner"/></a>
+				</div>
+
 			<?php endif; ?>
 
 			<p class="copyright">Copyright &copy; <?php echo date( 'Y' ); ?>, Easy Digital Downloads. <?php if( ! is_page( 'checkout' ) ) : ?>A project by <a href="<?php echo home_url( '/the-crew/' ); ?>">Pippin Williamson and Friends</a>.<?php endif; ?></p>
