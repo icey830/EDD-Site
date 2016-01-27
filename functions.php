2b9ddf959c587d40973f861934528d20c3effceb
Sean Davis <sdavis2702@gmail.com>
2016-01-27 13:55:04 -0800

    bust cache

--- functions.php
+++ functions.php
@@ -34,7 +34,7 @@
  * 1. Theme Setup
  * ----------------------------------------------------------- */
 
-define( 'EDD_SITE_VERSION', '2.1.8' );
+define( 'EDD_SITE_VERSION', '2.1.8-beta1' );
 
 /**
  * Set the content width.
