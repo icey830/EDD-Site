63ac0bcb37c8e3340e6a45d6ad68056c6c123f64
Sean Davis <sdavis2702@gmail.com>
2016-01-26 18:40:08 -0800

    fully remove original checkout error placement #833

--- functions.php
+++ functions.php
@@ -1512,10 +1512,11 @@ function eddwp_edd_is_activated() {
 /**
  * reposition checkout page form errors
  */
+remove_action( 'edd_ajax_checkout_errors', 'edd_print_errors' );
+remove_action( 'edd_purchase_form_before_submit', 'edd_print_errors' );
 function eddwp_reposition_checkout_errors() {
 	add_action( 'edd_purchase_form_before_submit', 'edd_print_errors', 999 );
 }
-remove_action( 'edd_ajax_checkout_errors', 'edd_print_errors' );
 add_action( 'init', 'eddwp_reposition_checkout_errors' );
 
 
