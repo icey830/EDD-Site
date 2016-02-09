18ae46c82eded5f8a20808abe74144814077fef6
Sean Davis <sdavis2702@gmail.com>
2016-02-09 14:28:15 -0800

    separate extensions and themes on product info page #744

--- page-templates/template-product-info.php
+++ page-templates/template-product-info.php
@@ -10,6 +10,7 @@ get_header(); ?>
 <div id="product-info-area" class="product-info-area page-section-gray full-width">
 	<div class="inner">
 		<div class="product-info-content page-section">
+			<h4 id="extensions">Extensions</h4>
 			<ul class="product-info-list">
 				<?php
 					$products = new WP_Query(
@@ -19,6 +20,65 @@ get_header(); ?>
 							'post_status'    => 'publish',
 							'orderby'        => 'title',
 							'order'          => 'ASC',
+							'tax_query'      => array(
+								'relation'   => 'AND',
+								array(
+									'taxonomy' => 'download_category',
+									'field'    => 'slug',
+									'terms'    => 'extensions'
+								),
+							)
+						)
+					);
+
+					while ( $products->have_posts() ) : $products->the_post();
+						?>
+							<li class="product-info-list-item">
+								<?php the_title( '<span class="product-title"><a href="' . home_url( '/downloads/' . $post->post_name ) . '" title="' . get_the_title() . '">', '</a></span>' ); ?>
+								<span class="product-version">
+									<?php
+										$version = get_post_meta( get_the_ID(), '_edd_sl_version', true );
+										if ( ! empty( $version ) ) :
+											echo $version;
+										else :
+											echo 'not versioned';
+										endif;
+									?>
+								</span>
+								<span class="product-author">
+									<?php
+										$dev = get_post_meta( get_the_ID(), 'ecpt_developer', true );
+										if ( ! empty( $dev ) ) :
+											echo 'by ' . $dev;
+										else :
+											echo 'no author';
+										endif;
+									?>
+								</span>
+							</li>
+						<?php
+					endwhile;
+					wp_reset_postdata();
+				?>
+			</ul>
+			<h4 id="themes">Themes</h4>
+			<ul class="product-info-list">
+				<?php
+					$products = new WP_Query(
+						array(
+							'post_type'      => 'download',
+							'posts_per_page' => -1,
+							'post_status'    => 'publish',
+							'orderby'        => 'title',
+							'order'          => 'ASC',
+							'tax_query'      => array(
+								'relation'   => 'AND',
+								array(
+									'taxonomy' => 'download_category',
+									'field'    => 'slug',
+									'terms'    => 'themes'
+								),
+							)
 						)
 					);
 
