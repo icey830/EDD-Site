5e7db863dd329d0112c033625e9efe69908b67fa
Sean Davis <sdavis2702@gmail.com>
2016-03-12 12:25:39 -0800

    restructure and redesign site footer #857

--- footer.php
+++ footer.php
@@ -36,50 +36,69 @@
 		</div>
 	<?php endif; ?>
 
-	<footer id="colophon" class="footer-area full-width" role="contentinfo">
+	<footer id="colophon" class="footer-area page-section-darkblue full-width" role="contentinfo">
 		<div class="inner">
 			<div class="site-footer">
 				<?php if ( ! is_404() && ( function_exists( 'edd_is_checkout' ) && ! edd_is_checkout() ) ) : ?>
 					<img class="sitting-edd" src="<?php echo get_template_directory_uri(); ?>/images/edd-sitting.png" />
 				<?php endif; ?>
 				<?php if ( ( function_exists( 'edd_is_checkout' ) && ! edd_is_checkout() ) && ! is_page( array( 130 ) ) ) : ?>
-					<div class="footer-columns clearfix">
-						<div class="footer-latest-posts">
-							<?php eddwp_get_latest_post(); ?>
+					<div class="footer-columns flex-container clearfix">
+						<div class="footer-latest-posts flex-three">
+							<?php eddwp_get_latest_post( 5 ); ?>
 						</div>
 
-						<div class="footer-support">
+						<div class="footer-support flex-three">
 							<h4>Need help?</h4>
 							<p>If you ever need help with EDD, you are encouraged to use our <a href="<?php echo home_url( '/support/' ); ?> ">support system</a> where you can always get your support questions answered. If you'd like to report a bug or have ideas for how to improve the plugin, please post it to our <a href="http://github.com/easydigitaldownloads/Easy-Digital-Downloads/issues">GitHub Issue Tracker</a>.</p>
 						</div>
 
-						<div class="footer-consultants">
+						<div class="footer-consultants flex-three">
 							<h4>Trusted Consultants</h4>
 							<p>We maintain a list of consultants that we recommend working with when it comes to managed support, customization, and setup help. If you are looking for additional help with EDD, consider these <a href="<?php echo home_url( '/consultants/' ); ?>">trusted consultants</a>.</p>
 						</div>
 					</div>
+				<?php endif; ?>
 
-					<div class="site-info">
+				<div class="footer-site-details flex-container clearfix">
+					<div class="copyright flex-two">
+						<?php if ( ( function_exists( 'edd_is_checkout' ) && ! edd_is_checkout() ) ) : ?>
+							<h4>Additional Resources</h4>
+						<?php endif; ?>
+						<p><span class="edd-copyright">Copyright &copy; <?php echo date( 'Y' ); ?> &middot; Easy Digital Downloads</span>
+							<?php
+
+								// do not show on checkout
+								if ( ( function_exists( 'edd_is_checkout' ) && ! edd_is_checkout() ) ) :
+									?>
+									&nbsp;&middot;&nbsp;a project by <a href="<?php echo home_url( '/the-crew/' ); ?>">Pippin Williamson and Friends</a>.
+									<?php
+									// EDD social profiles
+									$args = array(
+										'title'  => 'Connect with Easy Digital Downloads. ',
+										'wrap'   => false,
+										'square' => true,
+									);
+									eddwp_social_networking_profiles( $args );
+								endif;
+							?>
+						</p>
+					</div>
+					<?php if ( ( function_exists( 'edd_is_checkout' ) && ! edd_is_checkout() ) ) : ?>
+					<div class="site-info flex-two">
 						<?php if ( has_nav_menu( 'footer' ) ) : ?>
+							<h4>Additional Resources</h4>
 							<nav id="footer-menu" class="navigation-footer">
 								<?php wp_nav_menu( array( 'theme_location' => 'footer' ) ); ?>
-							</nav><!-- /#primary -->
+							</nav>
 						<?php endif; ?>
 
-						<div class="social clearfix">
-							<ul>
-								<li class="facebook"><a href="https://www.facebook.com/eddwp"></a></li>
-								<li class="twitter"><a href="https://twitter.com/eddwp"></a></li>
-								<li class="gplus"><a href="https://plus.google.com/111409933861760783237/posts"></a></li>
-								<li class="github"><a href="https://github.com/easydigitaldownloads/Easy-Digital-Downloads/"></a></li>
-							</ul>
-						</div>
-
-						<a class="godaddy-partner" href="https://garage.godaddy.com/wordpress-plugin-partner-program/directory/?cvosrc=advocacy.evangelism.WP3" rel="nofollow"><img src="https://garage.godaddy.com/wp-content/uploads/badges/godaddy-plugin-partner-program-badge.svg" title="GoDaddy Plugin Program Partner" alt="GoDaddy Plugin Program Partner"/></a>
+						<a class="godaddy-partner" href="https://garage.godaddy.com/wordpress-plugin-partner-program/directory/?cvosrc=advocacy.evangelism.WP3" rel="nofollow">
+							<img src="https://garage.godaddy.com/wp-content/uploads/badges/godaddy-plugin-partner-program-badge.svg" title="GoDaddy Plugin Program Partner" alt="GoDaddy Plugin Program Partner"/>
+						</a>
 					</div>
-
-				<?php endif; ?>
-				<p class="copyright">Copyright &copy; <?php echo date( 'Y' ); ?> &middot; Easy Digital Downloads<?php if ( ( function_exists( 'edd_is_checkout' ) && ! edd_is_checkout() ) ) : ?> &middot; A project by <a href="<?php echo home_url( '/the-crew/' ); ?>">Pippin Williamson and Friends</a>.<?php endif; ?></p>
+					<?php endif; ?>
+				</div>
 			</div>
 		</div>
 	</footer>
