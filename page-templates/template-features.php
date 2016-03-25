891776a6bbd901686a3e3fdac5e33a00814303ce
Sean Davis <sdavis2702@gmail.com>
2016-03-25 09:46:51 -0700

    introduce features page layout and styles #550

--- page-templates/template-features.php
+++ page-templates/template-features.php
@@ -8,45 +8,32 @@ get_header();
 the_post();
 ?>
 
-<div id="features-page-intro" class="features-page-section page-section-gray full-width">
+<div id="features-page-intro-area" class="features-page-intro-section page-section-gray full-width">
 	<div class="inner">
 		<div class="features-page-intro">
 			<div class="section-header">
 				<h2 class="section-title">Easy Digital Downloads <strong>Core Features</strong></h2>
-				<p class="section-subtitle">Easy Digital Downloads is a full-featured E-Commerce platform with all the tools and features you need to turn your WordPress website into a a digital store. To see for yourself, check out the features below.</p>
+				<p class="section-subtitle">Easy Digital Downloads is a full-featured eCommerce platform with all the tools and features you need to turn your WordPress website into a a digital store. Check out the features below and see for yourself.</p>
 			</div>
 		</div>
 	</div>
 </div>
 
-<div id="front-page-features" class="features-grid-two page-section-white full-width">
+<div id="features-page-area" class="features-page-section page-section-white full-width">
 	<div class="inner">
-		<div class="features-grid-two-content">
-			<div class="features-grid-content-sections flex-container">
-				<div class="edd-feature flex-two">
-					<h6><i class="fa fa-tag"></i>Discount Codes</h6>
-					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla tristique erat ac felis accumsan efficitur. Nam quis lorem et quam scelerisque sodales. Integer id ullamcorper magna.</p>
-				</div>
-				<div class="edd-feature flex-two">
-					<h6><i class="fa fa-shopping-cart"></i>Full Shopping Cart</h6>
-					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla tristique erat ac felis accumsan efficitur. Nam quis lorem et quam scelerisque sodales. Integer id ullamcorper magna.</p>
-				</div>
-				<div class="edd-feature flex-two">
-					<h6><i class="fa fa-download"></i>Unlimited File Downloads</h6>
-					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla tristique erat ac felis accumsan efficitur. Nam quis lorem et quam scelerisque sodales. Integer id ullamcorper magna.</p>
-				</div>
-				<div class="edd-feature flex-two">
-					<h6><i class="fa fa-bar-chart"></i>Download Activity Tracking</h6>
-					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla tristique erat ac felis accumsan efficitur. Nam quis lorem et quam scelerisque sodales. Integer id ullamcorper magna.</p>
-				</div>
-				<div class="edd-feature flex-two">
-					<h6><i class="fa fa-database"></i>WordPress REST API</h6>
-					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla tristique erat ac felis accumsan efficitur. Nam quis lorem et quam scelerisque sodales. Integer id ullamcorper magna.</p>
-				</div>
-				<div class="edd-feature flex-two">
-					<h6><i class="fa fa-line-chart"></i>Full Data Reporting</h6>
-					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla tristique erat ac felis accumsan efficitur. Nam quis lorem et quam scelerisque sodales. Integer id ullamcorper magna.</p>
-				</div>
+		<div class="features-page-content clearfix">
+			<div class="features-content">
+				<h6><i class="fa fa-tag"></i>Discount Codes</h6>
+				<ul>
+					<li>choose flat rate or percentage based</li>
+					<li>specify products to include or exclude</li>
+					<li>set automated start and end dates</li>
+					<li>require minimum cart total for discount use</li>
+					<li>limit discount usage by count or user</li>
+				</ul>
+			</div>
+			<div class="features-screenshot">
+				<img class="features-image" src="<?php echo get_template_directory_uri() . '/images/features-page/edd-discounts-screenshot.png'; ?>">
 			</div>
 		</div>
 	</div>
