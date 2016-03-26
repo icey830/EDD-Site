71725053637d327fa18030f84332b846d250c1b2
Sean Davis <sdavis2702@gmail.com>
2016-03-25 17:27:42 -0700

    change front page hero screenshot to match features screenshots #551

--- front-page.php
+++ front-page.php
@@ -11,16 +11,20 @@ get_header(); ?>
 
 <div id="front-page-hero" class="front-page-section page-section-blue full-width">
 	<div class="inner">
-		<div class="front-page-intro">
-			<div class="site-headline">
-				<span class="hero-subtitle">Say hello to the world's easiest way to</span>
-				<h1 class="hero-title">Sell Digital Downloads Through WordPress</h1>
+		<div class="front-page-intro clearfix">
+			<div class="hero-info">
+				<div class="site-headline">
+					<span class="hero-subtitle">Say hello to the world's easiest way to</span>
+					<h1 class="hero-title">Sell Digital Downloads Through WordPress</h1>
+				</div>
+				<p class="hero-cta">
+					<a class="hero-primary-cta-button" href="http://downloads.wordpress.org/plugin/easy-digital-downloads.latest-stable.zip?utm_source=home&utm_medium=button_2&utm_campaign=Download+Button"><i class="fa fa-cloud-download"></i>Download</a><br>
+					or <a class="hero-secondary-cta-link" href="https://easydigitaldownloads.com/demo/">view the demo</a>
+				</p>
 			</div>
-			<p class="hero-cta">
-				<a class="hero-primary-cta-button" href="http://downloads.wordpress.org/plugin/easy-digital-downloads.latest-stable.zip?utm_source=home&utm_medium=button_2&utm_campaign=Download+Button"><i class="fa fa-cloud-download"></i>Download</a><br>
-				or <a class="hero-secondary-cta-link" href="https://easydigitaldownloads.com/demo/">view the demo</a>
-			</p>
-			<img class="hero-screenshot" src="<?php echo get_template_directory_uri() . '/images/edd-reports-screenshot-edd.png'; ?>">
+			<div class="hero-thumb">
+				<img src="<?php echo get_template_directory_uri() . '/images/edd-reports-screenshot-home.png'; ?>">
+			</div>
 		</div>
 	</div>
 </div>
