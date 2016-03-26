c773feb7887e8f8425d0fb9fcb76b4298e531ae9
Sean Davis <sdavis2702@gmail.com>
2016-03-26 12:21:20 -0700

    adjust support pricing page styles

--- page-templates/template-support-pricing.php
+++ page-templates/template-support-pricing.php
@@ -11,28 +11,34 @@ get_header(); ?>
 		<div class="inner">
 			<div class="support-pricing-table-content clearfix">
 				<h2 class="section-title-alt">Priority Support</h2>
-				<div class="yearly-support support-type">
-					<h2 class="support-type-title">Yearly Access<span>Recommended</span></h2>
-					<div class="support-type-price">
-						<span class="support-price">$299</span>
+				<div class="flex-container">
+					<div class="yearly-support support-type flex-two">
+						<h2 class="support-type-title">Yearly Access <span>Recommended</span></h2>
+						<div class="support-type-info">
+							<div class="support-type-price">
+								<span class="support-price">$299</span>
+							</div>
+							<div class="support-type-description">
+								Get yearly access to Priority Support and always be at the front of the queue. We will provide technical support and even log into your site to solve problems. As a paid member, your responses will be answered as rapidly as possible.
+							</div>
+							<div class="support-type-cta">
+								<a class="button edd-submit blue" href="/support/register/?level=2">Sign up for yearly support</a>
+							</div>
+						</div>
 					</div>
-					<div class="support-type-description">
-						Get yearly access to Priority Support and always be at the front of the queue. We will provide advanced technical support and even log into your site to solve your problems. As a paid member, your responses will be answered as rapidly as possible.
-					</div>
-					<div class="support-type-cta">
-						<a class="button edd-submit blue" href="/support/register/?level=2">Sign up for yearly support</a>
-					</div>
-				</div>
-				<div class="fortyfive-day-support support-type">
-					<h2 class="support-type-title">45 Days Access</h2>
-					<div class="support-type-price">
-						<span class="support-price">$99</span>
-					</div>
-					<div class="support-type-description">
-						Get access to 45 days of in-depth setup assistance, trouble shooting, and access to Priority Support.
-					</div>
-					<div class="support-type-cta">
-						<a class="button edd-submit blue" href="/support/register/?level=1">Sign up for 45 days<span> of priority support</span></a>
+					<div class="fortyfive-day-support support-type flex-two">
+						<h2 class="support-type-title">45 Days Access</h2>
+						<div class="support-type-info">
+							<div class="support-type-price">
+								<span class="support-price">$99</span>
+							</div>
+							<div class="support-type-description">
+								Get access to 45 days of in-depth setup assistance, trouble shooting, and access to Priority Support.
+							</div>
+							<div class="support-type-cta">
+								<a class="button edd-submit blue" href="/support/register/?level=1">Sign up for 45 days<span> of priority support</span></a>
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
