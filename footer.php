dd2ad934ee27e165d4f49c08f4341d74515bc448
Sean Davis <sdavis2702@gmail.com>
2016-03-28 23:11:00 -0700

    remove unnecessary markup

--- footer.php
+++ footer.php
@@ -16,7 +16,7 @@
 		<div id="footer-newsletter" class="page-section-darkblue full-width">
 			<div class="inner">
 				<div class="newsletter-content clearfix">
-					<h3 class="footer-section-title">Easy Digital Downloads <strong>Email Newsletter</strong></h3>
+					<h3 class="footer-section-title">Easy Digital Downloads Email Newsletter</h3>
 					<p>Be the first to know about the latest updates and exclusive promotions from Easy Digital Downloads.</p>
 					<form id="pmc_mailchimp" class="clearfix" action="" method="post">
 						<div class="nl-name clearfix">
