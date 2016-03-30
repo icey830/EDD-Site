2157970f55d591f6ae8e3410fba8d284b4c64ff2
Sean Davis <sdavis2702@gmail.com>
2016-03-29 19:52:49 -0700

    swap png logo for svg and add png fallback support #890

--- assets/js/dev/theme.js
+++ assets/js/dev/theme.js
@@ -6,6 +6,28 @@
 
 	$(function () {
 
+		// svg fallback to png for unsupported browsers
+		function svgasimg() {
+			return document.implementation.hasFeature(
+			"http://www.w3.org/TR/SVG11/feature#Image", "1.1");
+		}
+
+		if (!svgasimg()){
+			var e = document.getElementsByTagName("img");
+			if (!e.length){
+			e = document.getElementsByTagName("IMG");
+			}
+			for (var i=0, n=e.length; i<n; i++){
+			var img = e[i],
+				src = img.getAttribute("src");
+			if (src.match(/svgz?$/)) {
+				/* URL ends in svg or svgz */
+				img.setAttribute("src",
+				img.getAttribute("data-fallback"));
+			}
+			}
+		}
+
 
 		// Simple Notices remove notice
 		$('.remove-notice').on('click', function() {
@@ -69,7 +91,7 @@
 
 		// Load the mobile nav menu
 		$('.fa.menu-toggle').on('click', function() {
-		    $('#primary').slideToggle();
+			$('#primary').slideToggle();
 		});
 
 		$(window).resize(function() {
@@ -101,10 +123,10 @@
 
 
 		// Account page expired license toggle
-        $('.expired-key-toggle').on('click',function(e){
-            e.preventDefault();
-            $('.toggle-keys-container').slideToggle();
-        });
+	$('.expired-key-toggle').on('click',function(e){
+		e.preventDefault();
+		$('.toggle-keys-container').slideToggle();
+	});
 
 
 		// Support
--- header.php
+++ header.php
@@ -43,11 +43,13 @@
 						endif;
 						if ( ( function_exists( 'edd_is_checkout' ) && ! edd_is_checkout() ) || empty( $cart_contents ) ) :
 							?>
-							<a href="<?php echo get_option( 'siteurl' ); ?>" class="logo-image"><img src="<?php echo get_template_directory_uri() ?>/images/logo.png" alt="Easy Digital Downloads" /></a>
+							<a href="<?php echo get_option( 'siteurl' ); ?>">
+								<img class="edd-logo" src="<?php echo get_stylesheet_directory_uri() . '/images/edd-logo.svg'; ?>" alt="Easy Digital Downloads" data-fallback="<?php echo get_stylesheet_directory_uri() . '/images/edd-logo.png'; ?>">
+							</a>
 							<?php
 						else :
 							?>
-							<img src="<?php echo get_template_directory_uri() ?>/images/logo.png" alt="Easy Digital Downloads" />
+							<img class="edd-logo" src="<?php echo get_stylesheet_directory_uri() . '/images/edd-logo.svg'; ?>" alt="Easy Digital Downloads" data-fallback="<?php echo get_stylesheet_directory_uri() . '/images/edd-logo.png'; ?>">
 							<?php
 						endif;
 					?>
