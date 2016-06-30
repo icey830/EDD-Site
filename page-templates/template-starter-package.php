bdffa797034b6cf72a7895ddc58a51050786e21c
Sean Davis <sdavis2702@gmail.com>
2016-06-30 11:09:38 -0700

    first pass at unresponsive Start Package styling #978

--- page-templates/template-starter-package.php
+++ page-templates/template-starter-package.php
@@ -16,7 +16,7 @@ the_post();
 				<h1 class="entry-title">Jump-start your store with the <strong>EDD <?php the_title(); ?></strong></h1>
 				<span class="entry-headline">Let's make this simple.</span>
 				<div class="package-details clearfix">
-					<p class="package-description">Easy Digital Downloads has over 250 extensions to choose from. Finding the ones you need for your store can be a difficult task. Use the form below to build a Starter Package from some of our most popular extensions.</p>
+					<p class="package-description">Easy Digital Downloads has over 100 extensions to choose from. Finding the ones you need for your store can be a difficult task. Use the form below to build a Starter Package from some of our most popular extensions.</p>
 					<p class="package-discount">Purchase through this form and receive an automatic<span>30% Discount</span></p>
 				</div>
 			</div>
