5804e7e8d8060125416d0b5d8bf7921f55836c1f
Kyle Maurer <kyle@realbigmarketing.com>
2016-08-15 16:29:33 -0400

    Got some stuff working. Now there's one facet showing. Still need to get two more facets in, document some stuff and tidy up.

--- taxonomy-download_category.php
+++ taxonomy-download_category.php
@@ -77,10 +77,12 @@ $download_term = $wp_query->get_queried_object();
 						</div>
 					</div>
 					<?php echo facetwp_display( 'template', 'downloads' );
+					echo facetwp_display( 'pager' );
 				} else {
 					include( 'edd_templates/download-grid.php' );
+					eddwp_paginate_links();
 				}
-				eddwp_paginate_links(); ?>
+				?>
 				<div class="third-party-extensions-section">
 					<p>View more extensions built by talented developers from the EDD community.</p>
 					<a class="edd-submit button blue" href="<?php echo home_url( '3rd-party-extensions' ); ?>"><i class="fa fa-plug"></i>3rd Party Extensions</a>
