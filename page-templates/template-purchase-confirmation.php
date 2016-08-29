2a55570341c2fcd3f5efa9acff3f7794215a61c5
Sean Davis <sdavis2702@gmail.com>
2016-08-28 21:35:35 -0700

    link relevant docs on purchase confirmation page #969

--- page-templates/template-purchase-confirmation.php
+++ page-templates/template-purchase-confirmation.php
@@ -28,17 +28,51 @@ get_header();
 						<div class="tab-content">
 							<div class="tab-pane active purchase-confirmation-tab-pane" id="tab1">
 								<h3>Thank you for your purchase!</h3>
-								<?php
-									while ( have_posts() ) : the_post();
-										the_content();
-									endwhile;
-								?>
+								<p>You will be sent an email within a few minutes containing download links to the items you purchased. You may also access your license keys and download your purchases from <a href="https://easydigitaldownloads.com/your-account/">your account</a>.</p>
+								<p>Your purchase means a lot to us! If you'd like to tell your friends about us, use the buttons below to share Easy Digital Downloads with the world.</p>
+								<div class="social-buttons">
+									<div>
+										<script type="text/javascript">
+											// <![CDATA[
+											!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
+											// ]]>
+										</script>
+									</div>
+									<div>
+										<a class="twitter-share-button" href="https://twitter.com/share" data-url="https://easydigitaldownloads.com/extensions" data-text="I've just purchased extensions from @eddwp for #WordPress">Tweet</a>
+										<a class="twitter-follow-button" href="https://twitter.com/eddwp" data-show-count="false">Follow @eddwp</a>
+									</div>
+									<div>
+										<div class="g-plusone" data-size="tall" data-annotation="none" data-href="https://easydigitaldownloads.com"></div>
+										<script type="text/javascript">
+											// <![CDATA[
+											(function() { var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true; po.src = 'https://apis.google.com/js/plusone.js'; var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s); })();
+											// ]]>
+										</script>
+									</div>
+									<div>
+										<iframe style="border: none; overflow: hidden; width: 450px; height: 21px;" src="//www.facebook.com/plugins/like.php?href=https%3A%2F%2Feasydigitaldownloads.com%2F&amp;send=false&amp;layout=button_count&amp;width=450&amp;show_faces=true&amp;action=like&amp;colorscheme=light&amp;font&amp;height=21&amp;appId=411242818920331" width="300" height="150" frameborder="0" scrolling="no"></iframe>
+									</div>
+								</div>
+								<?php echo do_shortcode('[edd_receipt]'); ?>
 							</div><!-- /.tab-pane -->
 							<div class="tab-pane documentation-tab-pane" id="documentation-tab">
 								<h3>Easy Digital Downloads Documentation</h3>
 								<p>Thanks again for your purchase. Now it's time for the fun part. We understand the building an eCommerce site takes a lot of work. We encourage you to use our documentation to assist with getting the most out of your plugins.</p>
-								<h5>Getting Started</h5>
+								<h5>Helpful Documentation</h5>
 								<ul class="documentation-list">
+									<?php
+										$payment = new EDD_Payment( $edd_receipt_args['id'] );
+										foreach( $payment->downloads as $download ) {
+											$title = get_the_title( $download['id'] );
+											$doc = get_post_meta( $download['id'], 'ecpt_documentationlink', true );
+											if ( $doc ) {
+												echo '<li>';
+													echo '<a href="' . $doc . '">' . $title . ' documentation</a>';
+												echo '</li>';
+											}
+										}
+									?>
 									<li><a href="http://docs.easydigitaldownloads.com/article/844-introduction">Basic Easy Digital Downloads Introduction</a></li>
 									<li><a href="http://docs.easydigitaldownloads.com/article/192-how-do-i-install-an-extension">How do I install an extension?</a></li>
 									<li><a href="http://docs.easydigitaldownloads.com/article/1002-can-i-upgrade-my-licenses">Can I upgrade my licenses?</a></li>
