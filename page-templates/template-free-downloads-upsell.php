9a52b2c3085d6717de1ab4e0e2b190059f70161d
Sean Davis <seandavis@Seans-iMac.attlocal.net>
2016-11-15 17:08:28 -0800

    adjust copy and logged out view #1042

--- page-templates/template-free-downloads-upsell.php
+++ page-templates/template-free-downloads-upsell.php
@@ -13,7 +13,7 @@ the_post();
 			<div class="free-downloads-header clearfix">
 				<h2 class="section-title-alt">Thanks! Your download will begin momentarily.</h2>
 				<div class="free-downloads-header-content">
-					<p>For your personal records, you will receive a confirmation email to the address you provided. If you have  any issues with your download, please feel free to <a href="<?php echo home_url( '/support/' ); ?>">contact our support</a> for assistance. You may also use the <a href="http://docs.easydigitaldownloads.com/">documentation</a> to assist with setup and configuration. Enjoy!</p>
+					<p>For your personal records, you will receive a confirmation email to the address you provided. If you have any issues with your download, please feel free to <a href="<?php echo home_url( '/support/' ); ?>">contact our support</a> for assistance. You may also use the <a href="http://docs.easydigitaldownloads.com/">documentation</a> to assist with setup and configuration. Enjoy!</p>
 				</div>
 				<div class="free-downloads-header-media">
 				</div>
@@ -25,7 +25,7 @@ the_post();
 		<div class="inner">
 			<div class="recommended-products-thanks clearfix">
 				<h2 class="section-title-alt">Don't leave just yet! There's more.</h2>
-				<p class="recommended-products-thanks-intro">Based on your past activity, we've created a list of extensions that other customers found useful. Have a look! If you're just getting started, we also have a handy tool for building a <a href="<?php echo home_url( '/starter-package/' ); ?>">custom starter package</a>.</p>
+				<p class="recommended-products-thanks-intro">Based on your past activity, we've created a list of extensions that you may find useful. Have a look! If you're just getting started, we also have a handy tool for building a <a href="<?php echo home_url( '/starter-package/' ); ?>">custom starter package</a>.</p>
 				<?php
 				if ( is_user_logged_in() ) :
 
@@ -49,6 +49,8 @@ the_post();
 					// build Recommended Products output based on the recent free download
 					// and the products from the customer's last purchase
 					echo do_shortcode('[recommended_products ids="' . $ids . '" user="true" count="6"]');
+				else :
+					echo do_shortcode('[recommended_products ids="167,28530" user="true" count="6"]');
 				endif;
 				?>
 			</div>
