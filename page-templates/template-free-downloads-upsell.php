f2d57fbeda30d329f8a4cb9c1f63eb7f1ece3be7
Sean Davis <seandavis@Seans-iMac.attlocal.net>
2016-11-16 12:13:45 -0800

    refactor code to pad RP recommendations

--- page-templates/template-free-downloads-upsell.php
+++ page-templates/template-free-downloads-upsell.php
@@ -37,13 +37,13 @@ the_post();
 					if ( ! empty( $downloads ) ) {
 						// list the product IDs for the returned purchases
 						$purchased = array();
-						foreach ($downloads as $ids) {
+						foreach ( $downloads as $ids ) {
 							$purchased[] = $ids->ID;
 						}
-						$purchased_ids = implode(',', $purchased);
 
 						// pad results with Stripe, Recurring Payments, FES, Software Licensing, MailChimp
-						$ids = $purchased_ids . ',167,28530,54874,4916,746';
+						$ids = array_unique( array_merge( $purchased, array( 167,28530,54874,4916,746 ) ) );
+						$ids = implode( ',', $ids );
 					} else {
 						// Stripe, Recurring Payments, FES, Software Licensing, MailChimp
 						$ids = '167,28530,54874,4916,746';
