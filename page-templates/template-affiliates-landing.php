8dc1cb5f39c06758b563fe6af42128dd6b9441a4
Sean Davis <sdavis2702@gmail.com>
2017-03-27 12:57:38 -0700

    build affiliate program features area #1108

--- page-templates/template-affiliates-landing.php
+++ page-templates/template-affiliates-landing.php
@@ -12,14 +12,49 @@ get_header(); ?>
 			<div class="hero-info">
 				<div class="hero-headline">
 					<span class="hero-subtitle">Refer customers and earn cash with the</span>
-					<h1 class="hero-title">Easy Digital Downlaods Affiliate Program</h1>
+					<h1 class="hero-title">Easy Digital Downloads Affiliate Program</h1>
 				</div>
 				<p class="hero-cta">
-					<a class="hero-primary-cta-button" href="<?php echo get_theme_mod( 'eddwp_download_core' ); ?>"><i class="fa fa-handshake-o" aria-hidden="true"></i>Start Earning</a><br>
-					or <a class="hero-secondary-cta-link" href="<?php echo home_url( '/downloads/' . $post->post_name ); ?>" title="<?php get_the_title(); ?>">see more information</a>
+					<a class="hero-primary-cta-button" href="<?php echo get_theme_mod( 'eddwp_download_core' ); ?>"><i class="fa fa-thumbs-o-up" aria-hidden="true"></i>Become an affiliate</a><br>
+					or <a class="hero-secondary-cta-link" href="<?php echo home_url( '/downloads/' . $post->post_name ); ?>" title="<?php get_the_title(); ?>">view our affiliate terms</a>
 				</p>
 			</div>
 			<div class="hero-thumb">
+				<img src="<?php echo get_template_directory_uri() . '/images/front-page/edd-reports-screenshot-home.png'; ?>">
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="features-grid-two page-section-white full-width">
+	<div class="inner">
+		<div class="features-grid-two-content">
+			<h2 class="section-title-alt">Why promote Easy Digital Downloads?</h2>
+			<div class="features-grid-content-sections flex-container">
+				<div class="edd-feature flex-two">
+					<h6><i class="fa fa-money" aria-hidden="true"></i>Cash for referrals</h6>
+					<p>Earn 10% commission on every single referral that results in a successful sale. With over 100 extensions to promote, there's no limit to how much you can earn by referring others to Easy Digital Downloads.</p>
+				</div>
+				<div class="edd-feature flex-two">
+					<h6><i class="fa fa-paint-brush" aria-hidden="true"></i>Attention-grabbing banners</h6>
+					<p>Don't waste time worrying about promotional tools. We provide dozens of high quality banners and HTML snippets complete with your affiliate ID, making setup and promotion a breeze.</p>
+				</div>
+				<div class="edd-feature flex-two">
+					<h6><i class="fa fa-pie-chart" aria-hidden="true"></i>Detailed statistics</h6>
+					<p>Fine-tune your marketing strategy based on real data found in your affiliate area. Referral data, conversion statistics, and earnings reports are always up-to-date and easily accessible.</p>
+				</div>
+				<div class="edd-feature flex-two">
+					<h6><i class="fa fa-calendar" aria-hidden="true"></i>30-day cookie lifetime</h6>
+					<p>After clicking your referral link, a cookie is stored in a potential customer's browser. This allows 30 days to come back and complete a purchase, increasing the liklihood of a successful conversion.</p>
+				</div>
+				<div class="edd-feature flex-two">
+					<h6><i class="fa fa-link" aria-hidden="true"></i>Direct Link Tracking</h6>
+					<p>From your <em>secure</em> site, link directly to Easy Digital Downloads without using your referral URL. Increase the chances of a successful referral using clean, SEO-friendly links.</p>
+				</div>
+				<div class="edd-feature flex-two">
+					<h6><i class="fa fa-bar-chart" aria-hidden="true"></i>Campaign tracking</h6>
+					<p>Create your own affiliate campaigns to better track the performance of your referral links. Separated campaign data is provided to help you determine which promotion tactics perform best.</p>
+				</div>
 			</div>
 		</div>
 	</div>
