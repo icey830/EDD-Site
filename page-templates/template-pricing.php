23331f8909d422f0e1a8d623f840975936a57891
Sean Davis <seandavis@Seans-Air.attlocal.net>
2017-05-12 11:19:29 -0700

    start design elements #1119

--- page-templates/template-pricing.php
+++ page-templates/template-pricing.php
@@ -9,24 +9,24 @@ get_header(); ?>
 
 <div id="pricing-table-area" class="page-section-white full-width">
 	<div class="inner">
-		<div class="pricing-table-content flex-container clearfix">
-			<div class="pricing-table-column flex-four">
-				<div class="table-heading">
+		<div class="pricing-table-content flex-container">
+			<div class="free-column pricing-table-column flex-four">
+				<div class="column-heading plain">
 					Free
 				</div>
 			</div>
-			<div class="pricing-table-column flex-four">
-				<div class="table-heading">
+			<div class="starter-package-column pricing-table-column flex-four">
+				<div class="column-heading enhanced-dark">
 					Starter Package
 				</div>
 			</div>
-			<div class="pricing-table-column flex-four">
-				<div class="table-heading">
-					Extensions Bundle
+			<div class="core-bundle-column pricing-table-column flex-four">
+				<div class="column-heading enhanced">
+					Core Bundle
 				</div>
 			</div>
-			<div class="pricing-table-column flex-four">
-				<div class="table-heading">
+			<div class="a-la-carte-column pricing-table-column flex-four">
+				<div class="column-heading">
 					À la carte
 				</div>
 			</div>
