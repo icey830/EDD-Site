6606c91af1dcc42931fbba0df2ad01a1e62c6512
Sean Davis <sdavis2702@gmail.com>
2017-11-20 05:34:43 -0800

    version bump

--- functions.php
+++ functions.php
@@ -8,7 +8,7 @@
  * Theme Setup
  * ----------------------------------------------------------- */
 
-define( 'EDD_SITE_VERSION', '2.4.2.2' );
+define( 'EDD_SITE_VERSION', '2.4.3' );
 define( 'EDD_INC', dirname(__FILE__) . '/includes/' );
 
 
